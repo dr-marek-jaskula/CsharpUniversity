51c1f968720abbef3588c571fc3b9cc33a0575be
EltinCreator <Marek.Jaskula1@gmail.com>
2022-03-24 17:44:46 +0100

    Model done

--- EFCore/Data models/Address.cs
+++ EFCore/Data models/Address.cs
@@ -10,3 +10,4 @@ public class Address
     public int? Flat { get; set; }
 }
 
+
---
+++ xUnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace xUnitTests;
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
