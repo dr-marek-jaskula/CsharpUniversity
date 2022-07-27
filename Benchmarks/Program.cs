b27e6a1436dffc0bc9a82a4b9454b7aad8c6506d
Marek <Marek@DESKTOP-8BJGCQ1>
2022-07-27 13:26:31 +0200

    CI test

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -1,17 +1,7 @@
-﻿using BenchmarkDotNet.Running;
-using Benchmarks.DateParser;
-using Benchmarks.Log;
-using Benchmarks.Reflections;
-using Benchmarks.Span;
-using Benchmarks.Strings;
-using Serilog;
-using Serilog.Events;
-
-//In this project we test the performance of some approaches described in other projects in current solution.
+﻿Console.WriteLine("In this project we test the performance of some approaches described in other projects in current solution");
 
 //StringBenchmarks.RunBenchmarks();
 //ReflectionBenchmarks.RunBenchmarks();
 //SpanBenchmarks.RunBenchmarks();
 //BenchmarkRunner.Run<DateParserBenchmarks>();
-
-LogBenchmarks.RunBenchmarks();
+//LogBenchmarks.RunBenchmarks();
