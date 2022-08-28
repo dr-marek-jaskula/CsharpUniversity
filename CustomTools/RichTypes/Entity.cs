19286e179ed911c52917686c03985d0af9f8fd69
Marek <Marek@DESKTOP-8BJGCQ1>
2022-08-28 22:14:52 +0200

    Add Entity

---
+++ CustomTools/RichTypes/Entity.cs
@@ -0,0 +1,51 @@
+﻿namespace CustomTools.RichTypes;
+
+public abstract class Entity : IEquatable<Entity>
+{
+    public Guid Id { get; private init; }
+
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    public static bool operator ==(Entity? first, Entity? second)
+    {
+        return first is not null && second is not null && first.Equals(second);
+    }
+    public static bool operator !=(Entity? first, Entity? second)
+    {
+        return !(first == second);
+    }
+
+    public bool Equals(Entity? other)
+    {
+        if (other is null)
+            return false;
+
+        if (other.GetType() != GetType())
+            return false;
+
+        return other.Id == Id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+            return false;
+
+        if (obj.GetType() != GetType())
+            return false;
+
+        if (obj is not Entity entity)
+            return false;
+
+        return entity.Id == Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode() * 41;
+    }
+}
+
